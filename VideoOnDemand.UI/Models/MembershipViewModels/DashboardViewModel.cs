50106e52462515fee79fa8471942a953e33ec048
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2018-05-21 21:45:33 +0200

    Lecture 6

---
+++ VideoOnDemand.UI/Models/MembershipViewModels/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoOnDemand.UI.Models.DTOModels;
+
+namespace VideoOnDemand.UI.Models.MembershipViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<List<CourseDTO>> Courses { get; set; }
+    }
+}
